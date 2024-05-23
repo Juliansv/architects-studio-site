c327bd25e832325e60ecd3d9315cb42d8f4176ae
Juliansv <juliansv22@gmail.com>
2024-05-24 00:35:21 +0200

    hero section. wip

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -3,8 +3,15 @@ const HeroSection = () => {
 		<section className="">
 			<div className="clip-path-0 relative flex h-screen max-h-screen overflow-hidden">
 				<div className="fixed left-0 top-0 z-0 h-full w-full">
-          <h1>HERO SECTION</h1>
-        </div>
+					<div className="slide-in-1000 animation-delay-500 relative flex h-[45vh] justify-center bg-stone-400 opacity-0">
+						<h1 className="fade-in-1000 animation-delay-1500 text-10xl absolute bottom-0 text-stone-200 opacity-0">
+							<span className="slide-in-1000 animation-delay-1600 opacity-0">box</span>{" "}
+							<span className="slide-in-1000 animation-delay-1700 opacity-0">concept</span>{" "}
+							<span className="slide-in-1000 animation-delay-1800 opacity-0">studio</span>
+						</h1>
+					</div>
+					<div></div>
+				</div>
 			</div>
 		</section>
 	);
