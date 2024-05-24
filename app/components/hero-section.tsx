3bfeaafe99d4770d3d1f2db8aaeebd57f73f9774
Juliansv <juliansv22@gmail.com>
2024-05-25 00:20:48 +0200

    Change animations duration

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -5,13 +5,13 @@ const HeroSection = () => {
 				<div className="fixed left-0 top-0 z-0 h-full w-full">
 					<div className="slide-in-1400 animation-delay-500 relative flex h-[45vh] justify-center bg-stone-400 opacity-0">
 						<div className="text-10xl absolute bottom-0 flex flex-row gap-10 text-stone-200">
-							<div className="title-slide-in-top-1000 animation-delay-1600 opacity-0">
+							<div className="title-slide-in-top-750 animation-delay-1600 opacity-0">
 								box
 							</div>{" "}
-							<div className="title-slide-in-top-1000 animation-delay-1700 opacity-0">
+							<div className="title-slide-in-top-750 animation-delay-1700 opacity-0">
 								concept
 							</div>{" "}
-							<div className="title-slide-in-top-1000 animation-delay-1800 opacity-0">
+							<div className="title-slide-in-top-750 animation-delay-1800 opacity-0">
 								studio
 							</div>
 						</div>
